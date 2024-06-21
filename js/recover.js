36f11b4a87e0ea17102cd58439fe0551c3c261c9
status <jliceaga@udesa.edu.ar>
2024-06-21 20:46:54 -0300

    recover terminado

--- js/recover.js
+++ js/recover.js
@@ -1,29 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", function (){
+    document.querySelector('#recoverForm').addEventListener('submit', function(e){
+        e.preventDefault();
+
     let email = document.querySelector("#email");
     let validacionEmail = document.querySelector(".validacionEmail");
-    email.addEventListener('input', function() {
-        if (email.value === '') {
-            validacionEmail.innerText = 'Por favor escriba su email';
-        } else {
-            validacionEmail.innerText = '';
-        }
-    })
-    
-   let confirm = document.querySelector("#confirm")
-   let recuperacionContraseña = document.querySelector(".recuperacionContraseña")
-   confirm.addEventListener('input', function() {
-        if (confirm.checked) {
-            recuperacionContraseña.innerText = ''
-        } else {
-            recuperacionContraseña.innerText = 'Por favor acepte el campo Quiero recuperar mi contraseña'
-        }
 
-        
-    
+    if (email.value === '') {
+        validacionEmail.innerText = 'Por favor escriba su Email';
+    } else {
+        validacionEmail.innerText = '';
+    }
 
+    let terminos = document.querySelector("#confirm")
+    let recuperarContraseña = document.querySelector(".recuperarContraseña")
+    if (!terminos.checked){
+        recuperarContraseña.innerText = 'Por favor acepte el campo Quiero recuperar mi contraseña'
+    } else {
+        recuperarContraseña.innerText = '';
+    }
 
-
-   })
-    
-    
-    })
+let alert = document.querySelector("#alert")
+let vuelta = document.querySelector(".vuelta")
+    if (email.value !== '' && terminos.checked) {
+    alert.innerText = 'Recibiras un email con las instrucciones para recuperar tu contraseña';
+    vuelta.style.display = 'block';
+    }
+})
+})
